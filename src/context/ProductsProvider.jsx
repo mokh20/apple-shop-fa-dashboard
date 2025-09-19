338b05895eaa46fafc73862eba7cc225964e6b1d
mokh20 <mohammad1379kh19@gmail.com>
2025-09-19 09:54:10 +0330

    refactor(ProductsProvider): replace Axios with Supabase client

--- src/context/ProductsProvider.jsx
+++ src/context/ProductsProvider.jsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import { createClient } from "@supabase/supabase-js";
 import {
   createContext,
   useCallback,
@@ -11,31 +11,16 @@ const ProductsContext = createContext();
 
 function ProductsProvider({ children }) {
   const [products, setProducts] = useState([]);
-
+  // API
   const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
   const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
-
-  const api = axios.create({
-    baseURL: `${SUPABASE_URL}/rest/v1`,
-    headers: {
-      apikey: SUPABASE_KEY,
-      Authorization: `Bearer ${SUPABASE_KEY}`,
-    },
-  });
+  const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
   const getProducts = useCallback(async () => {
-    const { data } = await api.get("/productsData?select=*");
+    const { data } = await supabase.from("productsData").select("*");
     setProducts(data);
   }, []);
 
-  // const getProducts = useCallback(async () => {
-  //   try {
-  //     const { data } = await axios.get("http://localhost:3006/productsData");
-  //     setProducts(data);
-  //   } catch (error) {
-  //     console.error("Error :", error);
-  //   }
-  // }, []);
   useEffect(() => {
     getProducts();
   }, [getProducts]);
